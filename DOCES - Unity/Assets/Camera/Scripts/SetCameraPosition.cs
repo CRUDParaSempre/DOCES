67e14ad5300504ab94006dd8c8f964312251e967
Marcelo Gomes <marceloanselmogomes@gmail.com>
2016-10-01 17:31:57 -0300

    Testando commit para o botão de novo jogo.

---
+++ DOCES - Unity/Assets/Camera/Scripts/SetCameraPosition.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetCameraPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void setCameraPosition(int x, int y, int z) {
+		Camera.main.transform.position = new Vector3 (x,y,z);
+	}
+}
