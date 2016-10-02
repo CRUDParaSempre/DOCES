ca8cc75e95b6385a895c948cbd2010fe3a0d914c
Marcelo Gomes <marceloanselmogomes@gmail.com>
2016-10-01 22:10:42 -0300

    Issue #33 (Novo Jogo) Botão de novo jogo. Animação Pronta. Muda a posição da camera para a seleção do personagem.

---
+++ DOCES - Unity/Assets/StartScreen/New Game Button/Scripts/ClickAnimationControl.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fungus;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Collider2D))]
+public class ClickAnimationControl : MonoBehaviour {
+	private Animator anim;
+	private int stage = 0;
+	private bool isMouseOver = false;
+	private bool clicked = false;
+	[SerializeField] private Fungus.Flowchart flowchart;
+	[SerializeField] private string blockName;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isMouseOver && Input.GetMouseButtonDown(0)) {
+			setMouseStatus (2);
+			clicked = true;
+
+		} else if(clicked && Input.GetMouseButtonUp(0)) {
+			setMouseStatus (1);
+			clicked = false;
+			flowchart.ExecuteBlock (blockName);
+		}
+	}
+
+	public void setMouseStatus(int mouseStatus) {
+
+		//mouse not over
+		if (mouseStatus == 0) {
+			anim.SetInteger ("stage",0);
+
+		//mouse over
+		} else if(mouseStatus == 1) {
+			anim.SetInteger ("stage",1);
+
+		//clicked
+		} else if (mouseStatus == 2) {
+			anim.SetInteger ("stage",2);
+		}
+	}
+
+	public void OnMouseEnter() {
+		setMouseStatus (1);
+		isMouseOver = true;
+	}
+
+	public void OnMouseExit() {
+		setMouseStatus (0);
+		isMouseOver = false;
+	}
+}
