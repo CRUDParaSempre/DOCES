b2f1db276f8919d274ba8e8d415378831cc94dd9
guilhermeresende <guilherme.resende.borges@gmail.com>
2016-10-05 12:20:39 -0300

    Botao do Github

---
+++ DOCES - Unity/Assets/StartScreen/MEU BOTAO/Script/MEUBOTAO.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MEUBOTAO : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void qualquernome(){
+		Application.OpenURL ("https://github.com/CRUDParaSempre/DOCES/wiki");
+	}
+}
