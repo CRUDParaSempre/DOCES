0acf65060839daeb5b3da7f03f67cfa34f0e2d58
Marcelo Gomes <marceloanselmogomes@gmail.com>
2016-10-17 17:31:14 -0200

    Animação da Camera - Issue #113
    Issue #113
    
    Adicionei uma nova função para o script SetCameraPosition que move a
    camera até aaquela posição moveCameraTo(Vector3 destination, float
    duration);
    
    Também consertei alguns bugs nos botão GIT e CREDITOS

--- DOCES - Unity/Assets/Camera/Scripts/SetCameraPosition.cs
+++ DOCES - Unity/Assets/Camera/Scripts/SetCameraPosition.cs
@@ -2,19 +2,36 @@
 using System.Collections;
 
 public class SetCameraPosition : MonoBehaviour {
+	private bool moving = false;
+	private float moveSpeed;
+	private Vector3 dest;
+	private Rigidbody2D body;
 
 	// Use this for initialization
 	void Start () {
-	
+		body = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (moving) {
+			if (Vector3.Distance (transform.position, dest) < 1f) {
+				body.velocity = Vector2.zero;
+				moving = false;
+			}
+		}
 	}
 
 
 	public void setCameraPosition(int x, int y, int z) {
 		Camera.main.transform.position = new Vector3 (x,y,z);
 	}
+
+	public void moveCameraTo(Vector3 dest, float duration) {
+		this.dest = dest;
+		body.velocity = (dest - transform.position) / duration ;
+		moving = true;
+	}
+
+	
 }
