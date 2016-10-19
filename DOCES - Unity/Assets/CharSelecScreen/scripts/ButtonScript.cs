de9e4f6507550e4239bc7aac2391a51580969dcf
Amanda Cristina Castro dos Santos <amanda.santos20@hotmail.com>
2016-10-18 22:15:22 -0200

    Adicionando a escolha de habilidades

---
+++ DOCES - Unity/Assets/CharSelecScreen/scripts/ButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Transform))]
+public class ButtonScript : MonoBehaviour {
+
+	public int offset;
+	public Transform m_Transform;
+	public Transform tm_Transform;
+
+	private MeterScript m_Script;
+	private TotalMeterScript tm_Script;
+
+	// Use this for initialization
+	void Start () {
+		m_Script = m_Transform.gameObject.GetComponent<MeterScript> ();
+		tm_Script = tm_Transform.gameObject.GetComponent<TotalMeterScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnMouseDown(){
+		int new_val = m_Script.value + offset;
+		int new_val_total = tm_Script.value - offset;
+		if (new_val <= 9 && new_val >= 0 && new_val_total <= 15 && new_val_total >= 0) {
+			m_Script.value = new_val;
+			tm_Script.value = new_val_total;
+		}
+	}
+}
