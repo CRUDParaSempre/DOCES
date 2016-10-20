743a35fa2d2883112e1cc9c29f1cb316a41a1e13
Amanda Cristina Castro dos Santos <amanda.santos20@hotmail.com>
2016-10-20 20:03:11 -0200

    Setas de seleção de roupas funcionando para calça e sapato

---
+++ DOCES - Unity/Assets/CharSelecScreen/scripts/ArrowBottomScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowBottomScript : MonoBehaviour {
+
+	public int offset;
+	public Transform b_Transform;
+
+	private BottomScript b_Script;
+
+	// Use this for initialization
+	void Start () {
+		b_Script = b_Transform.gameObject.GetComponent<BottomScript> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnMouseDown() {
+		int new_val = b_Script.value + offset;
+		b_Script.value = new_val;
+	}
+}
