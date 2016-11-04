3800f335c3d818b3b95d61f5e6c4938ee16a5633
Marcelo Gomes <marceloanselmogomes@gmail.com>
2016-11-04 18:36:05 -0200

    Arrumei a animação da loja
    Tava muito feia agora ta bunitinha

---
+++ DOCES - Unity/Assets/GameScreen/Shop/Scripts/ShopAnimationController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class ShopAnimationController : MonoBehaviour {
+	private bool shopEnabled = false;
+	[SerializeField] private Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void openShop() {
+		anim.SetBool ("shopOpen",true);
+	}
+
+	public void closeShop() {
+		anim.SetBool ("shopOpen",false);
+	}
+}
