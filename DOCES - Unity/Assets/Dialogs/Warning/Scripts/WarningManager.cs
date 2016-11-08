2942072e748a7dcf88f66577e4a174d85a684ce4
Marcelo Gomes <marceloanselmogomes@gmail.com>
2016-11-08 13:24:41 -0200

    Issue 171 - Warning Message
    Implementei o warning message.
    
    para utilizalo chame a função showWarning() ou showWarningAtPosition()
    no objeto  Warning Message Canvas.

---
+++ DOCES - Unity/Assets/Dialogs/Warning/Scripts/WarningManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WarningManager : MonoBehaviour {
+	[SerializeField] private Text descriptionText;
+	[SerializeField] private Canvas movableCanvas;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void showWarning(string text) {
+		this.gameObject.SetActive (true);
+		descriptionText.text = text;
+	}
+
+	public void showWarningAtPosition(string text, float x, float y) {
+		this.gameObject.SetActive (true);
+		movableCanvas.GetComponent<RectTransform> ().position = new Vector3 (x,y,0f);
+		descriptionText.text = text;
+	}
+}
