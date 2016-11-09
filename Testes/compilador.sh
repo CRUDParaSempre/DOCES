376359f528246cdec010336f07037a9b19b18ff0
Junio Cezar <juniocezar.dcc@gmail.com>
2016-11-09 16:03:56 -0300

    rodando travis

--- Testes/compilador.sh
+++ Testes/compilador.sh
@@ -2,18 +2,7 @@
 
 # Compilando Unity para Windows
 
-
-echo "==================== LS DOCES"
-ls ${PWD}
-echo "==================== LS DOCES Unity"
-ls ${PWD}/"DOCES - Unity"
-echo "==================== LS Builds"
-ls ${PWD}/"DOCES - Unity"/Builds/
-echo "==================== LS Build.*"
-ls ${PWD}/"DOCES - Unity"/Builds/*/*
-echo "==================== TEMP"
-ls ${PWD}/"DOCES - Unity"/Temp/
-
+mkdir Build
 
 project="DOCES - Unity"
 
@@ -24,11 +13,12 @@ echo "Compilando $project para ambientes webGL"
   -silent-crashes \
   -logFile ${PWD}/unity.log \
   -projectPath "${PWD}/DOCES - Unity" \
-  -buildTarget WebGL "${PWD}/Build/Web"  \
+  -buildTarget WebGL "${PWD}/Build"  \
   -quit
 
   #-buildOSXUniversalPlayer "${PWD}/game.app" \
 
+ls -LR > dados.log
 
 echo 'Logs do processo de compilacao'
 cat ${PWD}/unity.log
--- Testes/upload.sh
+++ Testes/upload.sh
@@ -11,29 +11,20 @@ echo "=============  Comprimindo arquivos gerados ================="
 echo ''
 echo ''
 echo "==================== LS DOCES"
-ls ${PWD}
-echo "==================== LS DOCES Unity"
-ls ${PWD}/"DOCES - Unity"
-echo "==================== LS Builds"
-ls ${PWD}/"DOCES - Unity"/Builds/
-echo "==================== LS Build.*"
-ls ${PWD}/"DOCES - Unity"/Builds/*/*
-echo "==================== TEMP"
-ls ${PWD}/"DOCES - Unity"/Temp/
 
-# tar -zcf web_build.tar ${PWD}/Build/Web
+# tar -zcf web_build.tar dados.
 
-# echo ''
-# echo ''
-# echo "=============  Enviando arquivo comprimido para servidor ================"
-# echo ''
-# echo ''
+ echo ''
+ echo ''
+ echo "=============  Enviando arquivo comprimido para servidor ================"
+ echo ''
+ echo ''
 
-# ftp -n $HOST <<END_SCRIPT
-# quote USER $USER
-# quote PASS $PASSWD
-# cd $REMOTEPATH
-# put web_build.tar
-# quit
-# END_SCRIPT
-# exit 0
+ ftp -n $HOST <<END_SCRIPT
+ quote USER $USER
+ quote PASS $PASSWD
+ cd $REMOTEPATH
+ put dados.log
+ quit
+ END_SCRIPT
+ exit 0
