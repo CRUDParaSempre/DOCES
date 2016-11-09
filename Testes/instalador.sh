f1a74d990cd8f096fa4770ba84bf1892b272299d
Junio Cezar <juniocezar.dcc@gmail.com>
2016-11-09 12:00:00 -0300

    Adicionando modulo compilacao OS X

--- Testes/instalador.sh
+++ Testes/instalador.sh
@@ -11,3 +11,10 @@ curl -o "Unity.pkg" "http://download.unity3d.com/download_unity/960ebf59018a/Mac
 echo 'Instalando a Unity versao 3.5.5f1'
 sudo installer -dumplog -package Unity.pkg -target /
 
+
+echo 'Baixando Modulo de compilacao OSX"
+curl -o "moduloOSX.pkg" "http://download.unity3d.com/download_unity/960ebf59018a/MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-5.3.5f1.pkg"
+
+
+echo 'Instalando modulo MAC para a Unity versao 3.5.5f1'
+sudo installer -dumplog -package moduloOSX.pkg -target /
