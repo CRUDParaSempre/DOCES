d46ae4300a9c8954c9801c66a6c1d90d94359997
Mariana <marianaossilva@gmail.com>
2016-11-10 20:51:43 -0200

    Script para os textos
    - Adicionada a tabela csv
    - Criado script para a tabela
    - Testando script getName (deu certo!!)

---
+++ DOCES - Unity/Assets/GameScreen/Cards/Scripts/getName.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class getName : MonoBehaviour {
+
+	public Text cardName;
+	public Cards_CSV cardTable;
+
+
+	// Use this for initialization
+	void Start () {
+
+		cardName = GetComponent<Text> ();
+		cardName.text = cardTable.Find_Nome_da_carta ("Casos de Uso").Nome_da_carta;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
