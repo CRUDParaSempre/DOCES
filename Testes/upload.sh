7c2314bfebb4349ef4b9b15ea6bb3f5354fbc863
Junio <juniocezar@Junios-MacBook-Pro.local>
2016-11-11 12:18:09 -0200

    Atualizando gerador webgl

--- Testes/upload.sh
+++ Testes/upload.sh
@@ -6,30 +6,33 @@ PASSWD="123456"
 REMOTEPATH='jogo'
 
 
- echo ''
- echo ''
- echo "=============  Enviando arquivo comprimido para servidor ================"
- echo ''
- echo ''
+# echo ''
+# echo ''
+# echo "=============  Enviando arquivo comprimido para servidor ================"
+# echo ''
+# echo ''
+
+# ftp -n $HOST <<END_SCRIPT
+# quote USER $USER
+# quote PASS $PASSWD
+# cd $REMOTEPATH
+# put dados.log
+# quit
+# END_SCRIPT
+
+echo '================ Zipando arquivo ==================='
+tar -zcf game.tar ${PWD}/'DOCES - Unity'/WEBGLRODA
+
+echo '================ Enviando arquivo game.tar ==================='
 
  ftp -n $HOST <<END_SCRIPT
  quote USER $USER
  quote PASS $PASSWD
  cd $REMOTEPATH
- put dados.log
+ put game.tar
  quit
  END_SCRIPT
 
-
- tar -zcf web_b.tar WebGL-Dist
-
- ftp -n $HOST <<END_SCRIPT
- quote USER $USER
- quote PASS $PASSWD
- cd $REMOTEPATH
- put web_b.tar
- quit
- END_SCRIPT
-
+echo '================ Enviado arquivo game.tar ==================='
 
  exit 0
