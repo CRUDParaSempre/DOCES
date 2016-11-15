cad43db0b5b4816a3ccd6715585f7468d2932689
Mariana <marianaossilva@gmail.com>
2016-11-15 18:00:32 -0200

    Começamos o quiz:
    - Já peguei a tabela preenchida

---
+++ DOCES - Unity/Assets/GameScreen/Quiz/Script/QuizManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class QuizManager : MonoBehaviour {
+
+	[SerializeField] private Text questionText=null;
+
+
+	[SerializeField] private Quiz_CSV quizCSV = null;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	
+	}
+
+
+	public void showQuiz(string difficulty) {
+
+
+
+		List<string> infos = quizCSV.getQuestions (difficulty, "01/01");
+
+		questionText.text = infos [0];
+
+	}
+
+	public void OnEnable(){
+		showQuiz (GameStateManager.Instance.newClientDifficulty);	
+		GameStateManager.Instance.setGameState (GameStateManager.GameState.GameCards);
+	}
+}
