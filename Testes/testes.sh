1b529b4cdb09551d2d89d1f768e3925f72f06636
Junio Cezar <juniocezar.dcc@gmail.com>
2016-11-16 23:33:39 -0200

    Atualizando .travis

---
+++ Testes/testes.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Rodando testes de integracao"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile /tmp/unity.log \
+  -executeMethod UnityTest.Batch.RunIntegrationTests \
+  -testscenes=GameScenes \
+  -projectPath "${PWD}/DOCES - Unity" \
+  -targetPlatform=WebGL \
+  -resultsFileDirectory=${PWD}/../ \
+  -quit
+
+#-buildTarget WebGL "${PWD}/Build/Web"
+
+echo "Log do teste"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+cat /tmp/unity.log
